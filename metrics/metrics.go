f1c902cde3025a33390f44ca1ec4a7a79ea12689
chennqqi <chennqqi@qq.com>
2018-08-17 16:14:28 +0800

    add metrics.go

---
+++ metrics/metrics.go
@@ -0,0 +1,36 @@
+package metrics
+
+import (
+	"expvar"
+	"fmt"
+	"net/http"
+)
+
+const (
+	METRIX_URL = "/debug/vars"
+)
+
+//Metrics Handler
+//http.HandleFunc(METRIX_URL, MetricsHandler)
+func MetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	first := true
+	report := func(key string, value interface{}) {
+		if !first {
+			fmt.Fprintf(w, ",\n")
+		}
+		first = false
+		if str, ok := value.(string); ok {
+			fmt.Fprintf(w, "%q: %q", key, str)
+		} else {
+			fmt.Fprintf(w, "%q: %v", key, value)
+		}
+	}
+
+	fmt.Fprintf(w, "{\n")
+	expvar.Do(func(kv expvar.KeyValue) {
+		report(kv.Key, kv.Value)
+	})
+	fmt.Fprintf(w, "\n}\n")
+}
