8bd54896d32500ac287a36a80e59f58d8f212e94
chennqqi <chennqqi@qq.com>
2019-04-18 15:09:10 +0800

    1

--- httputil/util.go
+++ httputil/util.go
@@ -1,12 +1,33 @@
 package httputil
 
 import (
+	"bufio"
 	"encoding/json"
+	"io"
 	"net/http"
+	"os"
 )
 
 func ReadJson(resp *http.Response, v interface{}) error {
 	defer resp.Body.Close()
 	d := json.NewDecoder(resp.Body)
 	return d.Decode(v)
+}
+
+func DownloadFile(url string, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	resp, err := http.Get(url)
+	if err != nil {
+		os.Remove(filename)
+		return err
+	}
+	//TODO: uncompress
+
+	io.Copy(bufio.NewWriter(file), resp.Body)
+	resp.Body.Close()
+	return nil
 }
