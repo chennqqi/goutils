252f8556a705d5ba0a976a7a42ecb25773f0c803
chennqqi <chennqqi@qq.com>
2019-06-19 16:37:32 +0800

    add gin utility for get header value

--- ginutils/gin.go
+++ ginutils/gin.go
@@ -66,3 +66,58 @@ func GetQueryBoolean(c *gin.Context, q string) (bool, error) {
 
 	return false, errors.Errorf("unexpect value=%v", queryValue)
 }
+
+func GetHeaderInt(c *gin.Context, q string) (int, error) {
+	var v int
+	queryValue := c.GetHeader(q)
+	if queryValue == "" {
+		return v, ErrNotExist
+	}
+	_, err := fmt.Sscanf(queryValue, "%d", &v)
+	return v, err
+}
+
+func GetHeaderInt64(c *gin.Context, q string) (int64, error) {
+	var v int64
+	queryValue := c.GetHeader(q)
+	if queryValue == "" {
+		return v, ErrNotExist
+	}
+	_, err := fmt.Sscanf(queryValue, "%d", &v)
+	return v, err
+}
+
+func GetHeaderfloat(c *gin.Context, q string) (float32, error) {
+	var v float32
+	queryValue := c.GetHeader(q)
+	if queryValue == "" {
+		return 0, ErrNotExist
+	}
+	_, err := fmt.Sscanf(queryValue, "%f", &v)
+	return v, err
+}
+
+func GetHeaderfloat64(c *gin.Context, q string) (float64, error) {
+	var v float64
+	queryValue := c.GetHeader(q)
+	if queryValue == "" {
+		return v, ErrNotExist
+	}
+	_, err := fmt.Sscanf(queryValue, "%f", &v)
+	return v, err
+}
+
+func GetHeaderBoolean(c *gin.Context, q string) (bool, error) {
+	queryValue := c.GetHeader(q)
+	if queryValue == "" {
+		return false, ErrNotExist
+	}
+	switch strings.ToLower(queryValue) {
+	case "true", "yes", "y":
+		return true, nil
+	case "false", "no", "n":
+		return false, nil
+	}
+
+	return false, errors.Errorf("unexpect value=%v", queryValue)
+}
